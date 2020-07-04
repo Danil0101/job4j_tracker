bbc7e82038f044e443e3fe0168fc9f06515721dc
Danil0101 <selyutin.d.g@gmail.com>
2020-07-04 07:18:33 +0300

    1.2. Вызов метода объекта [#308941] (Исправление)

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -12,6 +12,9 @@ public class Student {
     public static void main(String[] args) {
         Student petya = new Student();
         for (int i = 0; i < 3; i++) {
+            petya.music();
+        }
+        for (int i = 0; i < 3; i++) {
             petya.song();
         }
     }
