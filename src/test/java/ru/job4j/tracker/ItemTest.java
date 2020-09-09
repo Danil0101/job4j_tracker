d14d009bf7f576db3f84ad1cca2da60644e851be
Danil0101 <selyutin.d.g@gmail.com>
2020-09-09 14:27:24 +0300

    0. Сортировка [#308872]

---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+    @Test
+    public void SortItemsAscending() {
+        List<Item> items = Arrays.asList(
+                new Item(3),
+                new Item(1),
+                new Item(5)
+        );
+        Collections.sort(items, new SortItemsAscending());
+        assertEquals(1, items.get(0).getId());
+        assertEquals(3, items.get(1).getId());
+        assertEquals(5, items.get(2).getId());
+    }
+
+    @Test
+    public void SortItemsDescending() {
+        List<Item> items = Arrays.asList(
+                new Item(3),
+                new Item(1),
+                new Item(5)
+        );
+        Collections.sort(items, new SortItemsDescending());
+        assertEquals(5, items.get(0).getId());
+        assertEquals(3, items.get(1).getId());
+        assertEquals(1, items.get(2).getId());
+    }
+}
