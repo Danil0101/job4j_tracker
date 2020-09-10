4026b7d73b3472e5eaa194733a461515ee3a2275
Danil0101 <selyutin.d.g@gmail.com>
2020-09-10 16:51:21 +0300

    4. Сортировка номера [#308869] (fix)

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,36 +6,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        String numStrLeft = "";
-        String numStrRight = "";
-        int numLeft;
-        int numRight;
-        for (int i = 0; i < left.length(); i++) {
-            int curNumericValue = Character.getNumericValue(left.charAt(i));
-            if (curNumericValue >= 0 && curNumericValue <= 9) {
-                numStrLeft += curNumericValue;
-            } else {
-                break;
-            }
-        }
-        for (int i = 0; i < right.length(); i++) {
-            int curNumericValue = Character.getNumericValue(right.charAt(i));
-            if (curNumericValue >= 0 && curNumericValue <= 9) {
-                numStrRight += curNumericValue;
-            } else {
-                break;
-            }
-        }
-        try {
-            numLeft = Integer.parseInt(numStrLeft);
-        } catch (NumberFormatException e) {
-            numLeft = Integer.MAX_VALUE;
-        }
-        try {
-            numRight = Integer.parseInt(numStrRight);
-        } catch (NumberFormatException e) {
-            numRight = Integer.MAX_VALUE;
-        }
+        int numLeft = Integer.parseInt(left.split(". ")[0]);
+        int numRight = Integer.parseInt(right.split(". ")[0]);
         return Integer.compare(numLeft, numRight);
     }
 }
