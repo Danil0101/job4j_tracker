a7efefafb0e9431f4605a43d3c8e634afe7fdc94
Danil0101 <selyutin.d.g@gmail.com>
2020-09-17 10:57:32 +0300

    2.1.5 HashMap (2. Проверить две строчки на идентичность.)

---
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+
+/**
+ * 2. Проверить две строчки на идентичность.
+ * Даны две строки. Нужно проверить, что вторая строка получилась методом перестановок символов в первой строке.
+ */
+
+public class FreezeStr {
+    public static boolean eq(String left, String right) {
+        ArrayList<Character> leftList = new ArrayList<>();
+        for (char c : left.toCharArray()) {
+            leftList.add(c);
+        }
+
+        for (char c : right.toCharArray()) {
+            if (!leftList.contains(c)) {
+                return false;
+            }
+            leftList.remove(leftList.indexOf(c));
+        }
+        return true;
+    }
+}
