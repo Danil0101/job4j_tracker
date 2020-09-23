def1fd214ae87b805ea6910b0c262ddf6f1d0209
Danil0101 <selyutin.d.g@gmail.com>
2020-09-23 14:15:41 +0300

    8. Перенос методов в Stream API. [#308736]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -26,9 +26,9 @@ public class Card {
 
     @Override
     public String toString() {
-        return "Card{" +
-                "suit=" + suit +
-                ", value=" + value +
-                '}';
+        return "Card{"
+                + "suit=" + suit
+                + ", value=" + value
+                + '}';
     }
 }
