ae6a191782b97251172aa7a6681ec021095c25ed
Danil0101 <selyutin.d.g@gmail.com>
2020-09-23 08:40:48 +0300

    2. Список адресов [#308730] (added test)

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.stream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+    private List<Profile> profiles = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        profiles.add(new Profile("Moscow", "Mira", 1, 1));
+        profiles.add(new Profile("Kiev", "Mira", 1, 1));
+        profiles.add(new Profile("Riga", "Mira", 1, 1));
+    }
+
+    @Test
+    public void getAddressFromProfile() {
+        List<Address> rsl = new Profiles().collect(profiles);
+        List<Address> expected = List.of(
+                new Address("Kiev", "Mira", 1, 1),
+                new Address("Moscow", "Mira", 1, 1),
+                new Address("Riga", "Mira", 1, 1)
+                );
+        assertThat(rsl, is(expected));
+    }
+}
