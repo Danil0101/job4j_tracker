d02354bc707b7d7a30b183935e38f5791d6448e6
Danil0101 <selyutin.d.g@gmail.com>
2020-09-23 10:02:53 +0300

    5. Преобразование матрицы чисел в список чисел. [#308733]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -1,12 +1,12 @@
 package ru.job4j.stream;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -70,6 +70,18 @@ public class SchoolTest {
         List<Student> students = List.of(st1, st2);
         Map<String, Student> rsl = new School().convertToMap(students);
         Map<String, Student> expected = Map.of("Ivanov", st1, "Petrov", st2);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void matrixToList() {
+        List<List<Integer>> matrix = List.of(
+                List.of(1, 2, 3),
+                List.of(4, 5, 6),
+                List.of(7, 8, 9)
+        );
+        List<Integer> rsl = matrix.stream().flatMap(List::stream).collect(Collectors.toList());
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
         assertThat(rsl, is(expected));
     }
 }
