0f01c002ad71b0baf9c61b4120b574fb4677b6ce
Danil0101 <selyutin.d.g@gmail.com>
2020-10-02 09:16:42 +0300

    Job4jStream

---
+++ src/main/java/ru/job4j/stream/Job4jStream.java
@@ -0,0 +1,34 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Job4jStream {
+    private List<Object> list;
+
+    public static class Of {
+        private Job4jStream newJob4jStream;
+
+        public Of(Object ... objects) {
+            newJob4jStream = new Job4jStream();
+            newJob4jStream.list = new ArrayList<>(Arrays.asList(objects));
+        }
+
+        public Of filter(Predicate<Object> predicate) {
+            List<Object> newList = new ArrayList<>();
+            for (Object o : newJob4jStream.list) {
+                if (predicate.test(o)) {
+                    newList.add(o);
+                }
+            }
+            newJob4jStream.list = newList;
+            return this;
+        }
+
+        public List<Object> collect() {
+            return newJob4jStream.list;
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/stream/Job4jStreamTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class Job4jStreamTest {
+
+    @Test
+    public void shouldGreaterThanZero() {
+        List<Object> actual = new Job4jStream.Of(-2, -1, 0, 1, 2, 3).filter(o -> (int) o > 0).collect();
+        List<Object> expected = List.of(1, 2, 3);
+        assertThat(actual, is(expected));
+    }
+}
